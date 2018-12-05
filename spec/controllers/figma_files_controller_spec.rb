b2b2dd8a8056244a87c88700517bff3c8363fce7
Jonathan Fisher <jonathanzfisher@gmail.com>
2018-12-05 11:03:24 -0700

    test nodes action on figma files controller

--- spec/controllers/figma_files_controller_spec.rb
+++ spec/controllers/figma_files_controller_spec.rb
@@ -14,4 +14,22 @@ RSpec.describe FigmaFilesController, type: :controller do
     end
   end
 
+  describe "GET #nodes" do
+    it "returns http success with params id" do
+      get :nodes, { params: { id: 'PAalkr4Lvt9nTEJTmFMphB' } }
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("\"id\":\"0:0\",\"name\":\"Document\"")
+    end
+
+    it "returns a 404 without an id" do
+      get :nodes
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns a success with sort and filter params" do
+      get :nodes, { params: { id: 'PAalkr4Lvt9nTEJTmFMphB', sort: 'desc', filter: 'D' } }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
